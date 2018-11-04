1c3f2865f3509ea77549a0cff25b9038da7a464c
hachi <sp4847b9softone@gmail.com>
2018-11-04 19:17:36 +0900

    change enter bacvk ESC

--- keyboards/helix/rev2/keymaps/beggarl/keymap.c
+++ keyboards/helix/rev2/keymaps/beggarl/keymap.c
@@ -95,7 +95,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINUS,
     C_T(ENT),KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, L_M(QUOT),
     O(LSFT) ,KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC, KC_RBRC,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-    KC_MPLY, LO_S,    KC_LCTL, KC_LGUI, KC_LALT, KC_ESC,  KC_BSPC, KC_SPC,    KC_SPC,  KC_GRV,  KC_EQL,  KC_LALT, KC_RGUI, KC_ENT
+    KC_MPLY, LO_S,    KC_LCTL, KC_LGUI, KC_LALT, KC_BSPC, KC_ESC,  KC_SPC,    KC_GRV  ,KC_ENT,  KC_EQL,  KC_LALT, KC_RGUI, KC_SPC
     // tty切替に使うためにRALT -> LALT
     ),
 
